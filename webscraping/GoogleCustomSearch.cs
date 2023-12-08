b98c4e195843786e429af74a8fc501fd4849073d
abhijitjd003 <34028704+abhijitjd003@users.noreply.github.com>
2023-12-08 10:34:33 +0530

    Create GoogleCustomSearch.cs

---
+++ webscraping/GoogleCustomSearch.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedDataCleansing.Constants
+{
+    public static class GoogleCustomSearch
+    {
+        public static readonly string safe = "active";
+        public static readonly string lr = "lang_en";
+        public static readonly int filter = 1;
+        public static readonly int num = 2;
+        public static readonly int start = 1;
+    }
+}
